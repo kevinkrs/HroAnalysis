3cdef5f115ff9419cca0d6dee2d9c1c864a596c6
Joey <keeperjoey@gmail.com>
2018-04-06 14:52:13 +0200

    Database migrations

---
+++ database/migrations/2018_04_06_121139_create_feedbacks_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::dropIfExists('feedbacks');
+		   
+        Schema::create('feedbacks', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('feedback')->nullable();
+			$table->integer('grade');
+			$table->dateTime('timestamp_received_date');
+			
+			$table->string('course');
+            $table->foreign('course')->references('course_code')->on('courses')->onDelete('cascade');
+			
+			$table->string('location');
+			$table->foreign('location')->references('location_code')->on('locations')->onDelete('cascade');
+			
+			$table->integer('import')->unsigned();
+			$table->foreign('import')->references('id')->on('imports')->onDelete('cascade');
+			
+			$table->integer('school_year');
+	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}
