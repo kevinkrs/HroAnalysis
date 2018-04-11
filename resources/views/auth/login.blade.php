509b0a0a935da27bdb7f154aeffa1623447c6197
Hicham Wakrim <0897587@hr.nl>
2018-04-11 22:18:04 +0200

    Added register button to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,9 +60,11 @@
                         <button type="submit" class="btn btn-primary">
                             {{ __('Login') }}
                         </button>
-
                         <a class="btn btn-link" href="{{ route('password.request') }}">
                             {{ __('Forgot Your Password?') }}
+                        </a>
+                        <a class="btn btn-default" href="{{ route('register') }}">
+                            {{ __('Register') }}
                         </a>
                     </div>
                 </div>
